ca1a3c4e44d1221affe7352236d70872565e3088
Muhammad Ikhsan <muhammad.ikhsan.r@gmail.com>
2021-09-24 17:31:14 +0700

    feat: add google tag manager

--- resources/views/landing_page/main.blade.php
+++ resources/views/landing_page/main.blade.php
@@ -11,6 +11,14 @@
 	
 	<title>Grand Mahakam City</title>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P4T5RKW');</script>
+	<!-- End Google Tag Manager -->
+
     @include('landing_page.head')
 
 </head>
@@ -798,6 +806,11 @@
 		</footer><!-- /.main-footer -->
 	
 	</div><!-- /#wrap -->
+
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4T5RKW"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 
     @include('landing_page.foot')
 </body>
